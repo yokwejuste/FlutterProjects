8988f977e65f501f85d08bcd386a30f91ce1a70c
Steve Yonkeu <yokwejuste@yahoo.com>
2022-06-04 02:03:50 +0100

    updates: code cleaning in the main files
    Signed-off-by: Steve Yonkeu <yokwejuste@yahoo.com>

--- medic/lib/main.dart
+++ medic/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'HomePage/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,8 +7,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,23 +16,6 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: null,
-      body: null,
     );
   }
 }
