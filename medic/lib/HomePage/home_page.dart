57bf6666191054481644d72098946994bfd8ac5b
Steve Yonkeu <yokwejuste@yahoo.com>
2022-06-04 02:05:57 +0100

    feat: moving the home page from the main file
    Signed-off-by: Steve Yonkeu <yokwejuste@yahoo.com>

---
+++ medic/lib/HomePage/home_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:medic/AuthentificationPages/LoginPage/login_page.dart';
+import 'package:medic/HomePage/HomeCarousel/home_carousel.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  void count() {}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null,
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(top: 50),
+            child: SizedBox(
+              child: 
+              // Carousel(),
+              Image(
+                image: AssetImage('assets/images/bg.png'),
+              ), 
+            ),
+          ),
+          Expanded(child: Container()),
+          Flexible(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 50),
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  color: Colors.yellow,
+                  border: Border.all(
+                    color: Colors.yellow,
+                    width: 8,
+                  ),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: SizedBox(
+                  width: 400,
+                  height: 70,
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LoginPage(),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
