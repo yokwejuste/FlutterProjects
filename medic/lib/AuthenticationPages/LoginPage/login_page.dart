30b8b2376642386e8f09e0d023efb0b1b8139594
Steve Yonkeu <yokwejuste@yahoo.com>
2022-06-06 00:02:24 +0100

    feat: login page basic make out
    Signed-off-by: Steve Yonkeu <yokwejuste@yahoo.com>

---
+++ medic/lib/AuthenticationPages/LoginPage/login_page.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: null,
+      /* AppBar(
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(left: 20),
+            child: Icon(Icons.account_box),
+          )
+        ],
+        title: const Center(child: Text('Login')),
+      ), */
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+/*             Padding(
+              padding: const EdgeInsets.only(top: 10, bottom: 10),
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            labelText: 'First name',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 10.0,
+                    ),
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            labelText: "Last name",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ), */
+            Padding(
+              padding: const EdgeInsets.only(top: 120),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 10,
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
